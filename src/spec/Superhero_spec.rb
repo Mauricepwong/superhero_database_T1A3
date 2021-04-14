d318adec7875acd723727dde240d06c3d3d50ecf
Maurice Wong <cas0121202@coderacademy.edu.aulearclearlsgit initgit config --global usegit config --global user.email cas0121202@coderacademy.edu.au>
2021-04-14 18:40:33 +1000

    Add test cases

---
+++ src/spec/Superhero_spec.rb
@@ -0,0 +1,9 @@
+# require "./Superhero"
+
+# RSpec.describe "Superhero" do
+#     subject { Superhero.new }
+
+#     it "should respond to name" do
+#         expect{subject}.to  respond_to :name
+#     end
+# end
---
+++ src/spec/menumethods_spec.rb
@@ -0,0 +1,9 @@
+# require "./menumethods"
+
+# RSpec.describe "test create method" do
+#     subject {Superhero.new}
+
+#     it "should respond to name" do
+#         expect{subject}.to  respond_to :name
+#     end
+# end
